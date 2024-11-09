cfeaa3bd8b4ff0aeef318f6187297ba8ca7265b1
Your Name <you@example.com>
2024-11-09 23:06:50 +0530

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import gdown
+import os
 
+# Google Drive file ID for similarity.pkl
+file_id = "1m0QcQQgpUJxgSLEnOu1H6PNN09_x2QO-"
+download_url = f"https://drive.google.com/uc?id={file_id}"
+
+def fetch_similarity_matrix():
+    if not os.path.exists("similarity.pkl"):
+        # Download similarity.pkl from Google Drive if not already present
+        gdown.download(download_url, "similarity.pkl", quiet=False)
+    with open("similarity.pkl", "rb") as f:
+        similarity_matrix = pickle.load(f)
+    return similarity_matrix
+
+# Fetch the movie poster
 def fetch_poster(movie_id):
     response = requests.get(
         'https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(
@@ -10,6 +25,7 @@ def fetch_poster(movie_id):
     data = response.json()
     return "https://image.tmdb.org/t/p/w500" + data['poster_path']
 
+# Recommendation function
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -23,10 +39,12 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(movie_id))
     return recommended_movies, recommended_movie_posters
 
-# Load the movie data and similarity matrix
+# Load movie data
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Load the similarity matrix from Google Drive
+similarity = fetch_similarity_matrix()
 
 st.title('Movie Recommender System')
 
